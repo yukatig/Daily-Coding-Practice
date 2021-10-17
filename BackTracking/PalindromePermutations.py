aebb1907d20ff17043075dc139753ffb6e81a423
yukatig <yukati.g@gmail.com>
2021-10-16 22:17:49 -0700

    haha

--- BackTracking/PalindromePermutations.py
+++ BackTracking/PalindromePermutations.py
@@ -20,3 +20,28 @@ Constraints:
 1 <= s.length <= 16
 s contains only lowercase English letters.
 '''
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        
+        result = []
+        curr = []
+        self.backtrack(s, curr, result)
+        return result
+    
+    def isPalindrome(self, s):
+        return s == s[::-1]
+        
+    def backtrack(self, s, curr, result):
+        if not s:
+            result.append(curr)
+        else:
+            for i in range(1, len(s) + 1):
+                left = s[0:i]
+                right = s[i:len(s)]
+                if self.isPalindrome(left):
+                    self.backtrack(right, curr+[left], result)
+            
