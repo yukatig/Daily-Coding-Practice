5b07719d9c62e0c3eb3252f09b9da1bcfbcad9ca
yukatig <yukati.g@gmail.com>
2021-11-29 15:07:14 -0800

    backtracking!

---
+++ BackTracking/Permutation.py
@@ -0,0 +1,42 @@
+"""
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+Input: nums = [1]
+Output: [[1]]
+
+Constraints:
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+"""
+
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.permuteBacktrack(result, [], nums)
+        return result
+
+    def permuteBacktrack(self, result, temp, nums):
+        if len(nums) == 0:
+            if temp not in result:
+                result.append(temp)
+            return
+
+        for i in range(len(nums)):
+            newNums = nums[0:i] + nums[i + 1:]
+            step = temp + [nums[i]]
+            self.permuteBacktrack(result, step, newNums)
