fa5e7e9fcc8405b3dc3d9b454080ffc1316cac30
Yukati <yukati.g@gmail.com>
2021-12-24 22:03:34 -0800

    dp

---
+++ DynamicProgramming/LongestSubsequence.py
@@ -0,0 +1,39 @@
+"""
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements. For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+Example 1:
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Example 2:
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+
+Example 3:
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+ 
+Constraints:
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+"""
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        subseq = [1] * n
+
+        for i in range(1, n):
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    subseq[i] = max(subseq[i], subseq[j] + 1)
+
+        return max(subseq)
